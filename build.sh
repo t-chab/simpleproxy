43e5fbb3104defdb99deaf4168f1a5aca86ea6e4
tchab <tom@chabs.name>
2019-09-12 07:48:58 +0200

    Update build script.

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-GOPATH=$HOME/projects/go
+# Project is expected to be in $GOPATH/src/project-name
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+GOPATH="$(readlink -f "${CURRENT_DIR}"/../../)"
+
 GOROOT=$HOME/apps/sdk/go1.13
-
-GOARCH=amd64
-GOOS=linux
 
 PATH=$GOROOT/bin:$PATH
 
-go build simple-proxy
+# For linux be sure to have libgtk-3-dev and libappindicator3-dev installed
+env GOOS=linux GOARCH=amd64 go build simple-proxy
+env GOOS=linux GOARCH=arm64 go build simple-proxy
+env GOOS=darwin GOARCH=amd64 go build simple-proxy
+env GOOS=windows GOARCH=amd64 go build -ldflags -H=windowsgui simple-proxy
