384158577b2d2fa2f49f1aeb36584dcaf1b96dcb
Thomas Chabaud <chabaud.thomas@imsa.msa.fr>
2019-09-16 09:34:14 +0200

    Add Mac OS build specificities

--- build.sh
+++ build.sh
@@ -21,5 +21,12 @@ packr2
 # For linux be sure to have libgtk-3-dev and libappindicator3-dev installed
 env GOOS=linux GOARCH=amd64 go build simple-proxy
 env GOOS=linux GOARCH=arm64 go build simple-proxy
-env GOOS=darwin GOARCH=amd64 go build simple-proxy
 env GOOS=windows GOARCH=amd64 go build "${WIN_FLAGS}" simple-proxy
+
+# Mac Os executable can't be cross compiled. We need to be on Mac OS to do this ...
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    DARWIN_FLAGS="-gccgoflags=\"-DDARWIN -x objective-c -fobjc-arc\" -ldflags=\"framework Cocoa\""
+    go get github.com/machinebox/appify
+    env CC=gcc CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build ${DARWIN_FLAGS} simple-proxy
+    appify -name "Simple Proxy" -icon ./assets/simple-proxy.png simple-proxy
+fi
