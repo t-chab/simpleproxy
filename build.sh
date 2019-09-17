1f43cc91ee70fcddfa984387478c1f8deb87b707
Thomas Chabaud <chabaud.thomas@imsa.msa.fr>
2019-09-17 15:34:14 +0200

    Fix syntax error on compile flags. Add Mac OS packaging util.

--- build.sh
+++ build.sh
@@ -9,9 +9,6 @@ GOROOT=$HOME/apps/sdk/go1.13
 
 PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
-WIN_FLAGS="-ldflags -H=windowsgui"
-#WIN_FLAGS=""
-
 go get -u github.com/konsorten/go-windows-terminal-sequences
 go get -u github.com/gobuffalo/packr/v2/packr2
 
@@ -21,12 +18,16 @@ packr2
 # For linux be sure to have libgtk-3-dev and libappindicator3-dev installed
 env GOOS=linux GOARCH=amd64 go build simple-proxy
 env GOOS=linux GOARCH=arm64 go build simple-proxy
-env GOOS=windows GOARCH=amd64 go build "${WIN_FLAGS}" simple-proxy
+
+env GOOS=windows GOARCH=amd64 go build \
+    -ldflags -H=windowsgui \
+    simple-proxy
 
 # Mac Os executable can't be cross compiled. We need to be on Mac OS to do this ...
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    DARWIN_FLAGS="-gccgoflags=\"-DDARWIN -x objective-c -fobjc-arc\" -ldflags=\"framework Cocoa\""
+if [[ "${OSTYPE}" == "darwin"* ]]; then
     go get github.com/machinebox/appify
-    env CC=gcc CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build ${DARWIN_FLAGS} simple-proxy
+    env CC=gcc CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build \
+        -gccgoflags=-DDARWIN -x objective-c -fobjc-arc -ldflags=framework\ Cocoa \
+        simple-proxy
     appify -name "Simple Proxy" -icon ./assets/simple-proxy.png simple-proxy
 fi
